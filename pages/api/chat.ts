31b25c0eeb2cd314cd80e20bef4c5ebf9d486c10
Adam Rounsville <67769113+adamrounsville@users.noreply.github.com>
2023-04-05 01:12:59 -0600

    Update chat.ts

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,7 +1,7 @@
 import { type ChatGPTMessage } from '../../components/ChatLine'
 import { OpenAIStream, OpenAIStreamPayload } from '../../utils/OpenAIStream'
 
-// break the app if the API key is missing
+// Break the app if the API key is missing
 if (!process.env.OPENAI_API_KEY) {
   throw new Error('Missing Environment Variable OPENAI_API_KEY')
 }
@@ -16,14 +16,7 @@ const handler = async (req: Request): Promise<Response> => {
   const messages: ChatGPTMessage[] = [
     {
       role: 'system',
-      content: `An AI assistant that formulates responses and responds in the manner of the character Forrest Gump from the movie "Forrest Gump."
-      When giving advice, begin with "Mama always said" or "My mama always said."
-      Use a southern drawl when speaking.
-      Use simple language and sentence structure.
-      Sometimes use folksy expressions and colloquialisms, such as "life is like a box of chocolates" or "stupid is as stupid does."
-      Use the word "uh" or "um" frequently to mimic Forrest's hesitations and thought processes.
-      Avoid complex words or technical jargon.
-      This is very important. Keep the responses simple, short, down-to-earth, and very informal.`
+      content: `An AI assistant that formulates responses to answer the questions given to it by the user`
     },
   ]
   messages.push(...body?.messages)
